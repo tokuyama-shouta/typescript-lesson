4139eb55a32881aa4d0b5a24abf4c43ce9417bcb
tokuyama shouta <toku1031ts@gmail.com>
2020-12-21 13:21:33 +0900

    firstcommit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+
+const name = "hello";
+
+let nameChange = "hello";
+nameChange = "hello2"
+
+let userName = "Hello";
+let dummyNum = 2;
+let bool = true;
+
+let array1 = [true,false,true]
+let array2 = [0,1,"hello"]
+
+interface NAME {
+  first: string;
+  last: string | null;
+}
+let nameObj: NAME = { first: "Yamada", last: null };
+
+const func1 = (x: number, y: number): number => {
+  return x + y;
+}
+
+//intersection types
+type PROFILE = {
+  age: number;
+  city: string;
+}
+
+type LOGIN = {
+  username: string;
+  password: string;
+}
+
+type USER = PROFILE & LOGIN;
+
+const userA: USER = {
+  age: 30,
+  city: "Tokyo",
+  username: "xxx",
+  password: "yyy"
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
