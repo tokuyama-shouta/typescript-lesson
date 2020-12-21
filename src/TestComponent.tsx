3c6e2513be68b1bdcfa09093ad3eeae0fb42fdc4
tokuyama shouta <toku1031ts@gmail.com>
2020-12-21 15:54:12 +0900

    React useState

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 interface Props {
   text: string
 }
 
-const TestComponent:React.FC<Props> = (props) => {
+interface UserData {
+  id: number;
+  name: string;
+}
 
+const TestComponent:React.FC<Props> = (props) => {
+  const [count, setCount] = useState<number | null>(0);
+  const [user, setUser] = useState<UserData>({id: 1, name: "dummy"})
   return (
-    <div><h1>{props.text}</h1></div>
+    <div>
+      <h1>{props.text}</h1>
+      <h2>{count}</h2>
+    </div>
   )
 }
 
